fc582667d1f4b24daf291352384f95903267a682
abhishek <abhishekpagote3@gmail.com>
2024-09-26 23:57:55 +0530

    Abhishek Commit

--- app/src/main/java/com/vasp/ishwariabhi/Config.java
+++ app/src/main/java/com/vasp/ishwariabhi/Config.java
@@ -1,8 +1,5 @@
 package com.vasp.ishwariabhi;
 
 public interface Config {
-
-    static final String APP_SERVER_URL = "http://ftp.ishwarivegetables.in/PHP/";
-//    static final String APP_SERVER_URL = "http://ishwari.abhishekpagote.com/PHP/";
-//    static final String APP_SERVER_URL = "http://testing.ishwarivegetables.in/PHP/";
+    static final String APP_SERVER_URL = "http://testing.ishwarivegetables.in/PHP/";
 }
